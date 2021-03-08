db2f2bdc307355df1bfa5ddb6c75b35899707d04
Artem Stepanenko <artem@stepanenko.one>
2021-03-08 12:01:30 +0000

    add strong parameters for creation new instance of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,30 @@
+# frozen_string_literal: true
 class ArticlesController < ApplicationController
-    def index
-        @hello = "Cu CU Cu!!!"
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def show
-        @article = Article.find(params[:id])
-        @all_params = params
+  def show
+    @article = Article.find(params[:id])
+    @all_params = params
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render new
     end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
